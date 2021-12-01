998f4424a4557d7c02ee48be8a2dff22f31ba131
ZanyAlex <alextknott@gmail.com>
2021-12-01 15:46:34 +0000

    Added Clear grid functionality

---
+++ foo.js
@@ -0,0 +1,36 @@
+const body = document.body
+const container = document.querySelector(".container")
+function gridDraw() {
+    for (let i = 1; i<=256; i++) {
+        const gridDiv = document.createElement("div")
+        container.appendChild(gridDiv)
+        gridDiv.classList.add("square")
+        gridDiv.classList.add(i)
+    }
+}
+gridDraw()
+const squares = container.querySelectorAll("div.square");
+squares.forEach(i => {
+    i.addEventListener("mouseenter", mouseEnter)
+    i.addEventListener("mouseleave", mouseLeave)
+});
+
+
+function mouseEnter() {
+    this.classList.add("black")
+}
+
+function mouseLeave() {
+
+}
+
+const clearGridButton = document.querySelector("#clearGrid")
+clearGridButton.addEventListener("click", clearGridFunction)
+
+function clearGridFunction() {
+    const square = container.querySelectorAll("div.square")
+    for (i=0; i < square.length; i++) {
+        square[i].classList.remove("black")
+    }
+
+}

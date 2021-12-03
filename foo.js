9cc109f264e63e1109be4cfa4c65e06ac2f2979f
Zesty Alex <alextknott@gmail.com>
2021-12-03 20:29:43 +0000

    Added functionality for grids of different sizes to be created based on input

--- foo.js
+++ foo.js
@@ -1,6 +1,6 @@
 const body = document.body
 const container = document.querySelector(".container")
-function gridDraw() {
+/*function gridDraw() {
     for (let i = 1; i<=256; i++) {
         const gridDiv = document.createElement("div")
         container.appendChild(gridDiv)
@@ -8,7 +8,18 @@ function gridDraw() {
         gridDiv.classList.add(i)
     }
 }
-gridDraw()
+*/
+function gridDraw(input) {
+    container.style.setProperty("--grid-number", input);
+    for (let i = 1; i<=(input*input); i++) {
+        const gridDiv = document.createElement("div")
+        container.appendChild(gridDiv)
+        gridDiv.classList.add("square")
+        gridDiv.classList.add(i)
+    }
+}
+gridDraw(16);
+
 const squares = container.querySelectorAll("div.square");
 squares.forEach(i => {
     i.addEventListener("mouseenter", mouseEnter)

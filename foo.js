e1f9cafdd1fa43e09e2ad4c97ed76fd79db95ca6
Zesty Alex <alextknott@gmail.com>
2021-12-05 19:11:29 +0000

    Added 10% black each pass functionality

--- foo.js
+++ foo.js
@@ -23,9 +23,37 @@ function gridDraw(input) {
         squareSelector()
     }
 }
-
+/* Random RGB color Etch-A-Sketch Mode. 
 function mouseEnter() {
-    this.classList.add("black")
+    console.log(randomNumbers()[0])
+    this.style.backgroundColor = "rgb(" + (randomNumbers()[0]) + "," + (randomNumbers()[1]) + "," + (randomNumbers()[2]) + ")";
+    console.log(this.style.backgroundColor)
+    this.style.backgroundColor = (randomNumbers().randomNumberArray[0]),randomNumbers().randomNumberArray[1],randomNumbers().randomNumberArray[2];
+
+}
+*/
+
+/* Black 10% each time Etch-A-Sketch Mode. */
+function mouseEnter() {
+    let squareBgColor = window.getComputedStyle(this).getPropertyValue("background-color");
+    squareBgColor = squareBgColor,toString()
+    let rgbaValues = squareBgColor.split("(")
+    console.log(rgbaValues)
+    rgbaValues[1] = rgbaValues[1].replace(")", "")
+    console.log(rgbaValues)
+    rgbaValues = rgbaValues[1]
+    rgbaValues = rgbaValues.split(", ")
+    console.log(rgbaValues) 
+    if (typeof rgbaValues[3] != "undefined" ) {
+        rgbaValues[3] = (rgbaValues[3] - 0.1)
+        this.style.backgroundColor = "rgb(" + rgbaValues[0] + ","
+        + rgbaValues[1] + "," + rgbaValues[2] + "," + (rgbaValues[3]) + ")";
+    }
+    else {
+        rgbaValues[3] = 0.9
+        this.style.backgroundColor = this.style.backgroundColor = "rgb(" + rgbaValues[0] + ","
+        + rgbaValues[1] + "," + rgbaValues[2] + "," + (rgbaValues[3]) + ")";
+    }
 }
 
 
@@ -52,4 +80,12 @@ function createGrid() {
         input = prompt("How many squares per side would you like for the new grid? Must be from 1-100.");
     }
     gridDraw(input)
+}
+
+function randomNumbers() {
+    let ran1 = (Math.random()*255)
+    let ran2 = (Math.random()*255)
+    let ran3 = (Math.random()*255)
+    let randomNumberArray = [ran1,ran2,ran3]
+    return randomNumberArray
 }
